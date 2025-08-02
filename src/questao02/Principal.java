5e11d2cd3773918e67edfec0a9998ff0bceb67b1
RHlamerg <rh1620026@gmail.com>
2025-08-01 22:30:07 -0300

    Resolução da questão 02

---
+++ src/questao02/Principal.java
@@ -0,0 +1,11 @@
+package questao02;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o seu peso: ");
+        int x = entrada.nextFloat();
+    
+}
